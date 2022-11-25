5886a59bb40e53964054c1e9e9ca93e2718c5fe1
KaHeyon <93692881+KaHeyon@users.noreply.github.com>
2022-11-25 13:59:40 +0900

    seondsketch
    ggg

--- sketch.js
+++ sketch.js
@@ -1,13 +1,29 @@
 let musicFile;
 var song;
+var button;
 
 function setup()
 {
-    soundFormats('mp3','ogg');
-    musicFile=loadSoun('wham-last-christmas', loadMusic);
+    createCanvas(200,200);
+    //soundFormats('mp3','ogg');
+    song = loadSound('wham-last-christmas.mp3', loaded);
+    button = createButton('play');
+    button.mousePressed(togglePlaying);
+    background(51);
     //musicFile.play();
 }
 
-function loadMusic(){
-    musicFile.play();
+function togglePlaying(){
+    if(!song.isPlaying()){
+        song.play();
+        song.setVolume(0.3);
+        button.html('pause');
+    } else{
+        song.stop();
+        button.html('play');
+    }
+}
+
+function loaded(){
+    console.log('loaded');
 }
